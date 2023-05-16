afede2f7ac885005e7236c53c984fc8c3cae9abb
Ismael Medeiros <ismaelcmedeiros@gmail.com>
2023-05-16 17:57:04 -0300

    chore(script): improve execution script

--- benchmark.sh
+++ benchmark.sh
@@ -3,13 +3,25 @@ set -e
 
 mkdir -p results
 
-docker compose up -d
+echo "[1] Starting fuzzer and dependencies"
+docker compose up -d;
 
-docker build -t benchmark:1.0.0 --quiet .
+echo "[1.1] Wait for all containers to be healthy"
+while true; do
+  health_status=$(docker inspect --format='{{.State.Health.Status}}' dogefuzz_api);
 
-rm -f result.json
-touch result.json
+  if [ "$health_status" = "healthy" ]; then
+    echo "All containers are healthy";
+    break;
+  fi
 
+  sleep 5;
+done
+
+echo "[2] Building benchmark container image"
+docker build -t benchmark:1.0.0 --quiet .;
+
+echo "[3] Running benchmark"
 docker run \
     --network dogefuzz_benchmark \
     --network-alias benchmark \
@@ -18,6 +30,8 @@ docker run \
     -v "$PWD/result.json:/app/result.json" \
     -v "$PWD/results:/app/results" \
     benchmark:1.0.0 \
-    $@
+    $@;
 
-docker rm -f dogefuzz_benchmark > /dev/null
+echo "[4] Stopping all containers"
+docker rm -f dogefuzz_benchmark > /dev/null;
+docker compose down > /dev/null;
